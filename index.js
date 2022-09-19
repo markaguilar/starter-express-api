6b5c9e02d7e11f5001d34a2e69a3a3a59707db58
Mark Ferdinan Aguilar <markferdinan.mfa@gmail.com>
2022-09-19 23:02:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
@@ -7,12 +8,5 @@ app.all('/', (req, res) => {
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         // listen for request
-        app.listen(process.env.PORT, () => {
-            console.log('connected to db & listening on http://localhost:' + process.env.PORT)
-        })
-
-    })
-    .catch((err) => {
-        console.log(err)
-    })
+        app.listen(process.env.PORT || 3000)
 
