d050bcf9af80c0634be290e00034f5dea37e1df5
Mark Ferdinan Aguilar <markferdinan.mfa@gmail.com>
2022-09-19 22:53:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,15 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.status(200).json({ name: 'test' , desc: 'description'})
 })
-app.listen(process.env.PORT || 3000)
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for request
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on http://localhost:' + process.env.PORT)
+        })
+
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
